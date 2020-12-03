14f8c90bc16a19558e21fd76199cc78d5ae2dc35
Louis Aumaitre <louis.aumaitre@tiime.fr>
2020-12-03 11:00:22 +0100

    day 3

---
+++ advent/2020/day_3.py
@@ -0,0 +1,27 @@
+from advent.input_reader import read_map
+
+local_map = read_map('input/2020/day_3', {'.': 0, '#': 1})
+
+
+def how_many_trees(local_slope, start, vertical_value, horizontal_val):
+    x = start
+    y = 0
+    trees = 0
+    while y < len(local_slope):
+        trees += local_slope[int(y)][int(x % len(local_slope[y]))]
+        y += vertical_value
+        x += horizontal_val
+    return trees
+
+
+print('PART 1')
+print(how_many_trees(local_map, 0, 1, 3))
+
+print('PART 2')
+result = 1
+for y, x in [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]:
+    print(x, y)
+    new = how_many_trees(local_map, 0, y, x)
+    result *= new
+    print(new)
+print(result)
--- advent/input_reader.py
+++ advent/input_reader.py
@@ -6,3 +6,15 @@ def read_list_of_values(filename, value_type):
 def read_list_of_values_with_separators(filename, value_types, separator):
     with open(filename) as file:
         return [list(map(lambda ty, val: ty(val), value_types, line.split(separator))) for line in file.readlines()]
+
+
+def read_map(filename, code: dict):
+    with open(filename) as file:
+        result = [
+            list(map(lambda cr: code.get(cr, cr), line))
+            for line in file.readlines()
+        ]
+        for i, line in enumerate(result):
+            if line[-1] == '\n':
+                result[i] = line[:-1]
+        return result
